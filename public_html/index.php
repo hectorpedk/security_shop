b13b6a78328e6ba87aceff34d68ae2b3521c444f
Dmitri Segejenkov <lamukra@gmail.com>
2013-05-28 04:54:29 +0200

    Modified database class with PDO connection, corrected functions, Check TODO

--- public_html/index.php
+++ public_html/index.php
@@ -6,27 +6,23 @@
 	require_once(LIBRARY_PATH . "/templateFunctions.php");
     require_once(CLASSES_PATH . "/database.class.php");
 
-    $test = new Cls\Database();
-
-$database = $test->connect();
-$database = $test->tableExists('dimul');
-var_dump($database);
-
 
 /*
     Now you can handle all your php logic outside of the template
     file which makes for very clean code!
+
+	Code in this comment is not needed - Dimul
+
+	$setInIndexDotPhp = "I am set in the index.php file.";
+
+    $test = "This is in index.php that is included in different place";
+	
+	 Must pass in variables (as an array) to use in template
+	$variables = array(
+		'setInIndexDotPhp' => $setInIndexDotPhp,
+        'test'             => $test
+	);
 */
-	
-//	$setInIndexDotPhp = "I am set in the index.php file.";
-//
-//    $test = "This is in index.php that is included in different place";
-	
-	// Must pass in variables (as an array) to use in template
-//	$variables = array(
-//		'setInIndexDotPhp' => $setInIndexDotPhp,
-//        'test'             => $test
-//	);
 	
 	Lib\renderLayoutWithContentFile("home.php");
 
--- resources/classes/database.class.php
+++ resources/classes/database.class.php
@@ -26,44 +26,245 @@ class Database
      */
 
     /**
-     * @var object pdo Holds PDO object database connection after successful connection;
+     * @var PDO pdo Holds PDO object after successful connection to the database;
      */
-    private $pdo;             // Checks to see if the connection is active
-    private $result = array();
-
-    function __construct() {
-        $this->db_host = 'localhost';
-        $this->db_name = 'test';
-        $this->db_user = 'admin';
-        $this->db_pass = '31081987';
-
-    } // Results that are returned from the query
+    private $pdo;              // Checks to see if the connection is active
+    private $result = array(); // Results that are returned from the query
 
     /*
-     * Connects to the database, only one connection
-     * allowed
+     * Connects to the database
+     * Only one connection allowed
      */
-
-
-
     public function connect()
     {
-        if(!$this->pdo)
-        {
+        if( !$this->pdo ) {
             try {
-                // Data Source Name for PDO connection;
+                # Data Source Name for PDO connection;
                 $dsn = 'mysql:host=' . $this->db_host . ';dbname=' . $this->db_name;
-                // Connection to the database happens here;
-                $this->pdo = new PDO($dsn, $this->db_user, $this->db_pass);
-                // Setts PDO error reporting to throw exceptions;
-                $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                // Setts private var $link to true indicating that connection was created;
+                # Connection to the database happens here;
+                $this->pdo = new PDO( $dsn , $this->db_user , $this->db_pass );
+                # Setts PDO error reporting to throw exceptions;
+                $this->pdo->setAttribute( PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION );
+                # Setts private var $link to true indicating that connection was created;
                 return true;
-            // If exception was thrown store into variable $e and echo it out;
-            } catch (PDOException $e) {
+
+            } catch ( \PDOException $e ) {
+                # If exception was thrown store into variable $e and echo it out;
                 echo 'ERROR IN CONNECTION: ' . $e->getMessage();
+                return false;
             }
 
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Changes the new database, sets all current results to null
+     *
+     * @param $name     Name of the databse to change to
+     *
+     * @return bool     Returns true if condition is met
+     */
+    public function setDatabase( $name )
+    {
+            if( $this->pdo ) {
+                $this->pdo = null;
+                $this->db_name = $name;
+                $this->connect();
+                return true;
+            }
+    }
+
+    /**
+     * Checks to see if the table exists when performing queries
+     *
+     * @param $table    Table name to chekc in database
+     *
+     * @return bool|PDOException     Function returns true if execution of SQL statement was successful and false otherwise ( throws PDO exception $e )
+     */
+    private function tableExists ( $table )
+    {
+        try{
+
+            $stmt = $this->pdo->prepare( 'SHOW TABLES FROM ' . $this->db_name . ' LIKE "' . $table . '"' );
+            $stmt->execute();
+            $stmt->setFetchMode( PDO::FETCH_ASSOC );
+
+            while ( $rows = $stmt->fetch() ){
+
+                return ($stmt->rowCount() == 1 ? true : false);
+
+            }
+        } catch ( \PDOException $e ){
+            echo 'Something is wrong in tableExists(): ' . $e->errorInfo[2];
+            return false;
+        }
+
+    }
+
+    /**
+     * Selects information from the database.
+     * Required: table      ( the name of the table )
+     * Optional: columns    ( the columns requested, separated by commas )
+     *           where      ( column = value as a string )
+     *           order      ( column DIRECTION as a string )
+     *
+     * @param string   $table      Table name to select from database                               | required parameter
+     * @param string   $columns    Column names to selet from table provided in $table parameter    | default *
+     * @param string   $where      WHERE clause for sql statement                                   | default null
+     * @param string   $order      ORDER BY clause fro sql statement                                | default null
+     *
+     * @return bool|PDOException     Function returns true if execution of SQL statement was successful and false otherwise ( throws PDO exception $e )
+     */
+    public function select( $table , $columns = '*' , $where = null , $order = null )
+    {
+        $sql = 'SELECT ' . $columns . ' FROM ' . $table;
+
+        if( $where != null )
+            $sql .= ' WHERE ' . $where;
+        if( $order != null )
+            $sql .= ' ORDER BY ' . $order;
+
+        try {
+            /** @var PDOStatement $stmt Holds a prepared statement for PDO object to handle */
+            $stmt = $this->pdo->prepare( $sql );
+            $stmt->execute();
+
+            # Iteration through all rows that were return from the table
+            for ( $i = 0 ; $i < $stmt->rowCount(); $i++ ) {
+
+                # Associative array of rows from table
+                # $rows - where array_index is row number, array_key is column name, array_key_value is column name value
+
+                /** @var array $rows Holds arrays of associative arrays of row data */
+                $rows = $stmt->fetch( PDO::FETCH_ASSOC );
+
+                /** @var array $key  Holds all column names for each row in $rows variable*/
+                $keys = array_keys( $rows );
+
+                # Iterate through all the keys inside each row ( $rows variable )
+                for ( $x = 0; $x < count( $keys ) ; $x++ ) {
+
+                    # Sanitize keys to allow only alphanumeric values of column names
+                    if ( !is_int( $keys[$x] ) ){
+
+                        # Handle conditions when there is more than one row, less than one or none and only one
+                        if ( $stmt->rowCount() > 1 ) {
+
+                            $this->result[ $i ][ $keys[$x] ] = $rows[ $keys[$x] ];
+
+                        } elseif ( $stmt->rowCount < 1 ) {
+
+                            $this->result = null;
+
+                        } else {
+
+                            $this->result[ $keys[$x] ] = $rows[ $keys[$x] ];
+
+                        }
+
+                    }
+
+                }
+
+            }
+            return true;
+
+        } catch ( \PDOException $e ){
+            echo 'Something is wrong in select(): ' . $e->errorInfo[2];
+            return false;
+        }
+
+    }
+
+    /**
+     * Insert values into the table
+     *
+     * Required: table      (the name of the table)
+     *           values     (the values to be inserted)
+     * Optional: columns    (if values don't match the number of rows)
+     *
+     * @param string    $table      Table name to insert into
+     * @param array     $values     Array of values to insert into table
+     * @param array     $columns    Array of columns into which to insert values
+     *
+     * @return bool|PDOException
+     */
+    public function insert( $table , $values , $columns = null )
+    {
+        if( $this->tableExists( $table ) )
+        {
+            $insert = 'INSERT INTO ' . $table;
+
+            if( $columns != null ) {
+
+                for ( $x = 0 ; $x < count( $columns ) ; $x++ ){
+
+                    /* Statement below is the same as
+                        if( is_string( $columns[$x] ) )
+                            $columns[$x] = $columns[$x];
+                    */
+                    is_string( $columns[$x] ) ? $columns[$x] = $columns[$x] : null;
+
+                }
+
+                $columns = implode( ',' , $columns );
+                $insert .= ' (' . $columns . ')';
+
+            }
+
+            for( $i = 0 ; $i < count( $values ) ; $i++ ) {
+
+                if( is_string( $values[$i] ) )
+                    $values[$i] = '"' . $values[$i] . '"';
+
+            }
+
+            $values = implode( ',' , $values );
+            $insert .= ' VALUES (' . $values . ')';
+
+            try {
+                /** @var PDOStatement $stmt Holds a prepared statement for PDO object to handle */
+                $stmt = $this->pdo->prepare( $insert );
+                $stmt->execute();
+                return true;
+
+            } catch ( \PDOException $e ){
+                echo "Something is wrong in insert(): " . $e->errorInfo[2];
+                return false;
+            }
+        }
+    }
+
+    /*
+    * Deletes table or records where condition is true
+    * Required: table (the name of the table)
+    * Optional: where (condition [column =  value])
+    */
+    // TODO: Finish delete, update with PDO class
+    public function delete($table,$where = null)
+    {
+        if($this->tableExists($table))
+        {
+            if($where == null)
+            {
+                $delete = 'DELETE '.$table;
+            }
+            else
+            {
+                $delete = 'DELETE FROM '.$table.' WHERE '.$where;
+            }
+            $del = @mysql_query($delete);
+
+            if($del)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         else
         {
@@ -72,241 +273,71 @@ class Database
     }
 
     /*
-    * Changes the new database, sets all current results
-    * to null
-    */
-    public function setDatabase($name)
+     * Updates the database with the values sent
+     * Required: table (the name of the table to be updated
+     *           rows (the rows/values in a key/value array
+     *           where (the row/condition in an array (row,condition) )
+     */
+    public function update($table,$rows,$where)
     {
-            if($this->pdo)
+        if($this->tableExists($table)) {
+            // Parse the where values
+            // even values (including 0) contain the where rows
+            // odd values contain the clauses for the row
+            for($i = 0; $i < count($where); $i++) {
+                if($i%2 != 0)
+                {
+                    if(is_string($where[$i]))
+                    {
+                        if(($i+1) != null)
+                            $where[$i] = '"'.$where[$i].'" AND ';
+                        else
+                            $where[$i] = '"'.$where[$i].'"';
+                    }
+                }
+            }
+            $where = implode('',$where);
+
+
+            $update = 'UPDATE '.$table.' SET ';
+            $keys = array_keys($rows);
+            for($i = 0; $i < count($rows); $i++) {
+                if(is_string($rows[$keys[$i]]))
+                {
+                    $update .= $keys[$i].'="'.$rows[$keys[$i]].'"';
+                }
+                else
+                {
+                    $update .= $keys[$i].'='.$rows[$keys[$i]];
+                }
+
+                // Parse to add commas
+                if($i != count($rows)-1) {
+                    $update .= ',';
+                }
+            }
+            $update .= ' WHERE '.$where;
+            $query = @mysql_query($update);
+            if($query)
             {
-                $this->pdo = null;
-                $this->db_name = $name;
-                $this->connect();
                 return true;
             }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
     }
 
     /*
-    * Checks to see if the table exists when performing
-    * queries
+    * Returns the result set
     */
-    public function tableExists ( $table )
+    public function getResult()
     {
-
-
-        $stmt = $this->pdo->prepare('SHOW TABLES FROM :dbname LIKE ":table"');
-        $data = array (
-            'dbname' => $this->db_name,
-            'table' => $table
-        );
-        $stmt->execute($data);
-
-        $rows = $stmt->fetch(PDO::FETCH_ASSOC);
-
-
-
-//        if($tablesInDb)
-//        {
-//            if(mysql_num_rows($tablesInDb)==1)
-//            {
-//                return true;
-//            }
-//            else
-//            {
-//                return false;
-//            }
-//        }
-
+        return $this->result;
     }
-
-//    /*
-//    * Selects information from the database.
-//    * Required: table (the name of the table)
-//    * Optional: rows (the columns requested, separated by commas)
-//    *           where (column = value as a string)
-//    *           order (column DIRECTION as a string)
-//    */
-//
-//    public function select($table, $rows = '*', $where = null, $order = null)
-//    {
-//        $stmt = 'SELECT ' . $rows . ' FROM ' . $table;
-//
-//        if($where != null)
-//            $stmt .= ' WHERE '.$where;
-//        if($order != null)
-//            $stmt .= ' ORDER BY '.$order;
-//
-//        $query = @mysql_query($stmt);
-//        if($query)
-//        {
-//            $this->numResults = mysql_num_rows($query);
-//            for($i = 0; $i < $this->numResults; $i++)
-//            {
-//                $r = mysql_fetch_array($query);
-//                $key = array_keys($r);
-//                for($x = 0; $x < count($key); $x++)
-//                {
-//                    // Sanitizes keys so only alpha values are allowed
-//                    if(!is_int($key[$x]))
-//                    {
-//                        if(mysql_num_rows($query) > 1)
-//                            $this->result[$i][$key[$x]] = $r[$key[$x]];
-//                        else if(mysql_num_rows($query) < 1)
-//                            $this->result = null;
-//                        else
-//                            $this->result[$key[$x]] = $r[$key[$x]];
-//                    }
-//                }
-//            }
-//            return true;
-//        }
-//        else
-//        {
-//            return false;
-//        }
-//    }
-//
-//    /*
-//    * Insert values into the table
-//    * Required: table (the name of the table)
-//    *           values (the values to be inserted)
-//    * Optional: rows (if values don't match the number of rows)
-//    */
-//    public function insert($table,$values,$rows = null)
-//    {
-//        if($this->tableExists($table))
-//        {
-//            $insert = 'INSERT INTO '.$table;
-//            if($rows != null)
-//            {
-//                $insert .= ' ('.$rows.')';
-//            }
-//
-//            for($i = 0; $i < count($values); $i++)
-//            {
-//                if(is_string($values[$i]))
-//                    $values[$i] = '"'.$values[$i].'"';
-//            }
-//            $values = implode(',',$values);
-//            $insert .= ' VALUES ('.$values.')';
-//
-//            $ins = @mysql_query($insert);
-//
-//            if($ins)
-//            {
-//                return true;
-//            }
-//            else
-//            {
-//                return false;
-//            }
-//        }
-//    }
-//
-//    /*
-//    * Deletes table or records where condition is true
-//    * Required: table (the name of the table)
-//    * Optional: where (condition [column =  value])
-//    */
-//    public function delete($table,$where = null)
-//    {
-//        if($this->tableExists($table))
-//        {
-//            if($where == null)
-//            {
-//                $delete = 'DELETE '.$table;
-//            }
-//            else
-//            {
-//                $delete = 'DELETE FROM '.$table.' WHERE '.$where;
-//            }
-//            $del = @mysql_query($delete);
-//
-//            if($del)
-//            {
-//                return true;
-//            }
-//            else
-//            {
-//                return false;
-//            }
-//        }
-//        else
-//        {
-//            return false;
-//        }
-//    }
-//
-//    /*
-//     * Updates the database with the values sent
-//     * Required: table (the name of the table to be updated
-//     *           rows (the rows/values in a key/value array
-//     *           where (the row/condition in an array (row,condition) )
-//     */
-//    public function update($table,$rows,$where)
-//    {
-//        if($this->tableExists($table))
-//        {
-//            // Parse the where values
-//            // even values (including 0) contain the where rows
-//            // odd values contain the clauses for the row
-//            for($i = 0; $i < count($where); $i++)
-//            {
-//                if($i%2 != 0)
-//                {
-//                    if(is_string($where[$i]))
-//                    {
-//                        if(($i+1) != null)
-//                            $where[$i] = '"'.$where[$i].'" AND ';
-//                        else
-//                            $where[$i] = '"'.$where[$i].'"';
-//                    }
-//                }
-//            }
-//            $where = implode('',$where);
-//
-//
-//            $update = 'UPDATE '.$table.' SET ';
-//            $keys = array_keys($rows);
-//            for($i = 0; $i < count($rows); $i++)
-//            {
-//                if(is_string($rows[$keys[$i]]))
-//                {
-//                    $update .= $keys[$i].'="'.$rows[$keys[$i]].'"';
-//                }
-//                else
-//                {
-//                    $update .= $keys[$i].'='.$rows[$keys[$i]];
-//                }
-//
-//                // Parse to add commas
-//                if($i != count($rows)-1)
-//                {
-//                    $update .= ',';
-//                }
-//            }
-//            $update .= ' WHERE '.$where;
-//            $query = @mysql_query($update);
-//            if($query)
-//            {
-//                return true;
-//            }
-//            else
-//            {
-//                return false;
-//            }
-//        }
-//        else
-//        {
-//            return false;
-//        }
-//    }
-//
-//    /*
-//    * Returns the result set
-//    */
-//    public function getResult()
-//    {
-//        return $this->result;
-//    }
 }
